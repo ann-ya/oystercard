8c6f3eb49e8b4fcaa92b53e5199d3156e739e7cb
Anna <aejablonska66@gmail.com>
2021-09-27 16:13:46 +0100

    method & rspec deduct added

--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -11,12 +11,21 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
   
     it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change { subject.balance }.by 1
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
     end 
   
     it 'balance cannot exceed £90' do
       subject.top_up(card_limit)
       expect { subject.top_up(1) }.to raise_error "Cannot exceed limit of £#{card_limit}"
+    end
+  end
+
+  describe "#deduct" do
+    it { is_expected. to respond_to(:deduct).with(1).argument }
+    
+    it 'will deduct money from card' do
+    subject.top_up(10)
+    expect { subject.deduct 1 }.to change { subject.balance }.by -1
     end
   end
 
