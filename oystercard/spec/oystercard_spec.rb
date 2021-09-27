e399d25f128fb616699a7ec17295ea1e1668a2e3
Anna <aejablonska66@gmail.com>
2021-09-27 15:31:53 +0100

    method top_up added

--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -1,2 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
+  it "gives a new oystercardwith a balance" do
+    expect(subject.balance).to eq 0
+  end
+
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+  end
+
 end
